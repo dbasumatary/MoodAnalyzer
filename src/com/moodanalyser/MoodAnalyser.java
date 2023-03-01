5af14b543f0e0ee15b79d7ebb9047c221b36290e
dbasumatary <basudhondip@gmail.com>
2023-03-01 21:47:22 +0530

    Refactored: If user provides invalid mood then handle exception

--- src/com/moodanalyser/MoodAnalyser.java
+++ src/com/moodanalyser/MoodAnalyser.java
@@ -4,7 +4,14 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MoodAnalyser {
-    //Use Case 1: Analyse and respond Happy or Sad mood if a message is given
+    private static String message;
+
+    //Constructor with message as argument
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    //UC1: Analyse and respond Happy or Sad mood if a message is given
     public static String moodAnalyse(String message){
 
         //Taking two patterns Happy and Sad and matching them with message
@@ -23,12 +30,40 @@ public class MoodAnalyser {
         return mood;
     }
 
-    public static void main(String[] args) {
-        System.out.println("Analysing the mood of a person.");
 
-        //This will match the string with the pattern and generate the corresponding mood
-        //I have written a message regarding my mood
-        String mood = moodAnalyse("I am feeling Sad");
+    //UC2: Handle exception if the user provides invalid mood message
+    public static String invalidMoodAnalyser(){
+
+        //Initially the mood string is empty
+        String mood = "";
+        try{
+            //Taking two patterns Happy and Sad and matching them with message
+            Pattern pattern1 = Pattern.compile("Happy");
+            Pattern pattern2 = Pattern.compile("Sad");
+            Matcher matcher1 = pattern1.matcher(message);
+            Matcher matcher2 = pattern2.matcher(message);
+            if (matcher1.find()){
+                mood = "Happy";
+            } else if (matcher2.find()){
+                mood = "Sad";
+            }
+        }
+        // Catch exception if the user provides invalid mood message
+        //Here the output mood will be Happy
+        catch (NullPointerException e) {
+            mood =  "Happy";
+        }
+        return mood;
+    }
+    public static void main(String[] args) {
+        System.out.println("Analysing the mood of a person if null is given as mood");
+
+        //The input message is given as null
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+
+        //This will match the string with the pattern and generate the null input
+        String mood = moodAnalyser.invalidMoodAnalyser();
         System.out.println("The mood of the person is "+ mood);
     }
+
 }
--- src/com/moodanalyser/MoodAnalyserTest.java
+++ src/com/moodanalyser/MoodAnalyserTest.java
@@ -7,15 +7,22 @@ public class MoodAnalyserTest {
     //Happy mood message should return happy
     @Test
     public void whenGivenMessageIsHappyShouldReturnHappy(){
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        MoodAnalyser moodAnalyser = new MoodAnalyser("Happy");
         String mood = moodAnalyser.moodAnalyse("Happy");
         Assert.assertEquals("HAPPY", mood);
     }
+
     //Sad mood message should return sad
     @Test
     public void whenGivenMessageIsSadShouldReturnSad(){
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        MoodAnalyser moodAnalyser = new MoodAnalyser("Sad");
         String mood = moodAnalyser.moodAnalyse("Sad");
         Assert.assertEquals("SAD", mood);
+    }
+    @Test
+    public void whenGivenNullMoodShouldReturnHappy(){
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String mood = moodAnalyser.invalidMoodAnalyser();
+        Assert.assertEquals("HAPPY", mood);
     }
 }
